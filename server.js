e90ff7b3ffd9707b9b086c87ac64c0a4d42f496b
Garrett Lockhart <lockhartphotography1@gmail.com>
2022-10-19 21:36:05 -0600

    added basic file structure and route structure

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const routes = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 
 // setting up express middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
